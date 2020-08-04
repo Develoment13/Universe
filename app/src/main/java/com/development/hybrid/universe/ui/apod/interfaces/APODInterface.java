559454fd0212dab8fac4e6e97587b29cbd1433c7
Leonardo <lpalmaizaguirre@gmail.com>
2020-08-04 09:02:13 -0600

    Creating Interfaces for the class APOD

--- app/src/main/java/com/development/hybrid/universe/ui/apod/interfaces/APODInterface.java
+++ app/src/main/java/com/development/hybrid/universe/ui/apod/interfaces/APODInterface.java
@@ -1,4 +1,28 @@
 package com.development.hybrid.universe.ui.apod.interfaces;
 
+import com.development.hybrid.universe.ui.apod.model.APODModel;
+
 public interface APODInterface {
+
+    interface view{
+        void showProgress();
+        void hideProgress();
+        void hideApod();
+        void showApod();
+        void showApodDetails();
+        void fetchApodDetails();
+        void showDataFetchError(String error);
+        void onReloadData();
+    }
+
+    interface presenter{
+        void fetchData();
+        void onSuccess(APODModel apodModel);
+        void onFailure(String failure);
+    }
+
+    interface interactor{
+        void onRemoteFetch(final APODInterface.presenter listener);
+    }
+
 }
